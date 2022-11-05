59c596afdc8d825e540bda39b3a13eeef7fac6a1
Artem Zagarov <artem.zagarov@gmail.com>
2022-11-05 20:17:49 +0200

    added nginx for review-service

--- review-service/index.js
+++ review-service/index.js
@@ -7,7 +7,7 @@ const __dirname = path.resolve()
 
 dotenv.config()
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 // app.use(express.static(path.join(__dirname, "client", "build")))
 //
