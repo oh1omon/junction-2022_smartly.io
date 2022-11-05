0f2b077002ee6820286ab53f077162138cf984d8
Kuchteq <maniuchta3@gmail.com>
2022-11-05 10:35:38 +0200

    Themecolor gray

--- front/src/routes/Landing.js
+++ front/src/routes/Landing.js
@@ -9,7 +9,7 @@ export default function Landing() {
         <h1 className="landing_logo">Co.Mixer</h1>
         <img className="landing_image" src="/landingmain.jpg"/>
       </div>
-        <Box width={"17%"} marginLeft={"3%"} height={"fit-content"} paddingBottom={"2rem"}>
+        <Box width={"17%"} marginLeft={"3%"} height={"fit-content"} paddingBottom={"2rem"} >
         <Typography variant="body1" gutterBottom>
         Co.Mixer is meant to reveal the true side of your business. Create a short professional, promotional video effortlessly with only one click. The AI is going to 
       </Typography>
--- front/src/theme/MainTheme.js
+++ front/src/theme/MainTheme.js
@@ -1,16 +1,26 @@
-import { createTheme } from '@mui/material/styles';
-import { orange } from '@mui/material/colors';
+import { createTheme } from "@mui/material/styles";
+import { orange } from "@mui/material/colors";
 
 let theme = createTheme({
   palette: {
     primary: {
       main: orange[700],
-      contrastText: "#fff" //button text white instead of black
+      contrastText: "#fff", //button text white instead of black
     },
     secondary: {
       main: "#ffffff",
     },
-
+    grayed: {
+      main: "#f5f5f5",
+    },
+  },
+  typography: {
+    h5: {
+      fontSize: 22,
+      marginBottom: 15,
+      marginLeft: 30,
+      fontWeight: 600,
+    },
   },
 });
 
